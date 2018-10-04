9e493604d1351a294d816d55ecae7e2da7aa78ba
FinnLeduc <FinnLeduc@users.noreply.github.com>
2018-10-04 15:36:01 -0400

    added screenshots

--- MovingImagesFinnLeduc/main.lua
+++ MovingImagesFinnLeduc/main.lua
@@ -17,12 +17,24 @@ local backgroundImage = display.newImageRect("Images/background.png" , 2048, 153
 -- character image with width and height
 local beetleship = display.newImageRect("Images/beetleship.png" , 200, 200)
 
+local octopus = display.newImageRect("Images/octopus.png" , 200, 200)
+
 -- set the image to be transparent
 beetleship.alpha = 0
+
+--set the scale of the octopus to change direction
+octopus.xScale = -1
+
+-- set the image to opaque
+octopus.alpha = 1
 
 -- set the initial x and y position of beetleship
 beetleship.x = 0
 beetleship.y = display.contentHeight/3
+
+-- set the initial x and y position of the octopus
+octopus.x = 1048
+octopus.y = display.contentHeight * 3/4
 
 -- Function: MoveShip
 -- Input: this function accepts an event listener
@@ -30,10 +42,26 @@ beetleship.y = display.contentHeight/3
 -- Description: This function adds the scroll speed to the X-value of the ship
 local function MoveShip(event)
 	 -- add the scroll speed to the x-value of the ship
-	 beetleship.x = beetleship + scrollSpeed
+	 beetleship.x = beetleship.x + scrollSpeed
 	 -- change the transparency of the ship every time it moves so that it fades in
 	 beetleship.alpha = beetleship.alpha + 0.01 
 end	 
 
+
+-- Function: MoveShip
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This function adds the scroll speed to the X-value of the ship
+
+local function MoveOctopus(event)
+	 -- add the scroll speed to the x-value of the ship
+	 octopus.x = octopus.x - scrollSpeed
+	 -- change the transparency of the ship every time it moves so that it fades in
+	 octopus.alpha = octopus.alpha - 0.0001 
+end	 
+
 -- MoveShip will be called over and over again
 Runtime:addEventListener("enterFrame" , MoveShip)
+
+-- MoveOctopus will be called over and over again
+Runtime:addEventListener("enterFrame" , MoveOctopus)
