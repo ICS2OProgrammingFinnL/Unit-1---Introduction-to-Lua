a1b1899f45df7ba9e34cc25135935004babf1605
FinnLeduc <43419123+FinnLeduc@users.noreply.github.com>
2018-10-16 14:31:46 -0400

    updated ntf

--- NumericTextFieldsFinnL/main.lua
+++ NumericTextFieldsFinnL/main.lua
@@ -1,7 +1,59 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+--Title: Numeric Text Field
+--Name: Finn
+--Course: ICS20
+--This program displays a math question and asks the user to answer in a numeric text field terminal.
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- Hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- sets the background colour
+display.setDefault("background" , 200/255, 50/255, 100/255)
+
+
+--------------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+--------------------------------------------------------------------------------------------
+
+-- create my local variables
+local questionObject
+local correctObject
+local numericField
+local randomNumber1
+local randomNumber2
+local userAnswer
+local correctAnswer
+
+--------------------------------------------------------------------------------------------
+--LOCAL FUNCTIONS
+--------------------------------------------------------------------------------------------
+
+local function AskQuestion() 
+	-- generate 2 random numbers between a max. and a min. number
+	randomNumber1 = math.random(0, 10)
+	randomNumber2 = math.random(0, 10)
+
+
+	correctAnswer = randomNumber1 + randomNumber2
+
+	-- create question in text object
+	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+
+end
+
+local function HideCorrect()
+	correctObject.isVisible = false
+	AskQuestion()
+end
+
+local function NumericFieldListener(event)
+
+	-- user begins editing "numeric field"
+	if ( event.phase == "began") then
+		---clear text field
+		event.target.text = ""
+
+		elseif event.phase == "submitted" then 
+			-- when the answer is (enter key is pressed) set user input to user's answer
+			userAnswer = tonumber(event.target.text)
