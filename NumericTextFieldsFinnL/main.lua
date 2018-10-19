7c609048c666ecb87d1ce5a21eb0ec3930a504cd
FinnLeduc <43419123+FinnLeduc@users.noreply.github.com>
2018-10-19 14:32:52 -0400

    edtiing code

--- NumericTextFieldsFinnL/main.lua
+++ NumericTextFieldsFinnL/main.lua
@@ -24,7 +24,20 @@ local randomNumber1
 local randomNumber2
 local userAnswer
 local correctAnswer
+local incorrectAnswer
+local randomOperator
+local startingPoints = 0
+local points
 
+
+
+
+
+--variables for the  timer
+local totalSeconds = 5
+local secondsLeft
+
+--variables for the
 --------------------------------------------------------------------------------------------
 --LOCAL FUNCTIONS
 --------------------------------------------------------------------------------------------
@@ -33,17 +46,48 @@ local function AskQuestion()
 	-- generate 2 random numbers between a max. and a min. number
 	randomNumber1 = math.random(0, 10)
 	randomNumber2 = math.random(0, 10)
+	randomOperator = math.random(1,3)
 
+	if (randomOperator == 1) then
+	
+		correctAnswer = randomNumber1 + randomNumber2
 
-	correctAnswer = randomNumber1 + randomNumber2
+		-- create question in text object
+		questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
 
-	-- create question in text object
-	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+	elseif (randomOperator == 2) then
 
+		correctAnswer = randomNumber1 - randomNumber2
+
+		--create question in text object
+		questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. " = "
+
+	elseif (randomOperator == 3) then
+
+		correctAnswer = randomNumber1 * randomNumber2
+
+		--create question in text object
+		questionObject.text = randomNumber1 .. " * " .. randomNumber2 .. " = "
+	end
 end
+
+
+
+
+
+
+
+
+
+	
 
 local function HideCorrect()
 	correctObject.isVisible = false
+	AskQuestion()
+end
+
+local function HideIncorrect()
+	incorrectObject.isVisible = false
 	AskQuestion()
 end
 
@@ -57,3 +101,57 @@ local function NumericFieldListener(event)
 		elseif event.phase == "submitted" then 
 			-- when the answer is (enter key is pressed) set user input to user's answer
 			userAnswer = tonumber(event.target.text)
+
+			-- if the users answer and the correct answer are the same:
+			if (userAnswer == correctAnswer) then 
+				correctObject.isVisible = true
+				startingPoints = startingPoints + 1
+				timer.performWithDelay(2000, HideCorrect)
+
+				--display the points going up
+				points.text = " Points = " .. startingPoints
+			else
+				--if they aren't the same
+				incorrectObject.isVisible = true
+				timer.performWithDelay(2000, HideIncorrect)
+			end
+
+			event.target.text = ""
+		end
+    end
+
+---------------------------------------------------------------------------------------------
+--OBJECT CREATION
+--------------------------------------------------------------------------------------------
+
+--displays a  question and sets the colour
+questionObject = display.newText("", display.contentWidth/3, display.contentHeight/2, nil, 50 )
+questionObject:setTextColor(155/255, 100/255, 100/255)
+
+points = display.newText("Points = " .. startingPoints , display.contentWidth/1.4, display.contentHeight/2, nil, 50 )
+points:setTextColor(155/255, 100/255, 100/255)
+
+--create the correct text object and make it visible
+correctObject = display.newText ("Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
+correctObject:setTextColor(255/255, 10/255, 50/255)
+correctObject.isVisible = false
+
+
+incorrectObject = display.newText ("Incorrect!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
+incorrectObject:setTextColor(255/255, 10/255, 50/255)
+incorrectObject.isVisible = false
+
+
+--create numeric field 
+numericField = native .newTextField( display.contentWidth/2, display.contentHeight/2, 150, 80)
+numericField.inputType = "number"
+
+--add thier event listener for the numeric field
+numericField:addEventListener( "userInput", NumericFieldListener )
+
+----------------------------------------------------------------------------------------------
+--FUNCTION CALLS
+---------------------------------------------------------------------------------------------
+
+-- call the function to ask the question
+AskQuestion()
--- TouchReactFinn/main.lua
+++ TouchReactFinn/main.lua
@@ -6,13 +6,20 @@
 -----------------------------------------------------------------------------------------
 
 -- set background colour
-display.setDefault ("background", 50/255, 155/255, 100/255)
+display.setDefault ("background", 50/255, 100/255, 100/255)
 
 --hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
 -- create blue button, its position and make it visible
-local bluebutton = display.newImageRect("Images/BlueButton.png" , 198,96)
-bluebutton.x = display.contentWidth/2
-bluebutton.y = display.contentHeight/2
-BlueButton
+local blueButton = display.newImageRect("Images/BlueButton.png" , 198,96)
+blueButton.x = display.contentWidth/2
+blueButton.y = display.contentHeight/2
+blueButton.isVisible = true
+
+
+-- create red button, its position and make it invisible
+local redButtton = display.newImageRect("Images/RedButton.png" , 198,96)
+redButton.x = display.contentWidth/2
+redButton.y = display.contentHeight/2
+redButton.isVisible = false
